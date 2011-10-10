6bc9ef43c95e9ef8682f57d4e872e28601df88e4
Dmytrii Nagirniak <dnagir@gmail.com>
2011-10-11 09:43:08 +1100

    add digest option to CLI runner

--- lib/guard/rails-assets/cli_runner.rb
+++ lib/guard/rails-assets/cli_runner.rb
@@ -2,10 +2,14 @@ module Guard
   class RailsAssets::CliRunner
     def initialize(options={})
       @rails_env = (options[:rails_env] || 'test').to_s
+      @digest    = options[:digest]
+      @digest    = true if @digest.nil?
     end
 
     def compile_assets
-      system "RAILS_ENV=#{@rails_env} bundle exec rake assets:clean assets:precompile"
+      task = "assets:precompile"
+      task += ":nondigest" unless @digest
+      system "RAILS_ENV=#{@rails_env} bundle exec rake assets:clean #{task}"
     end
   end
 end
